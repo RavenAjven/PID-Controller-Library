9a59c83c503d20587f4d330517777db340b3c2af
Ivan <vpiroman@gmail.com>
2021-09-14 20:24:29 +0300

    Add files via upload

--- pidreg.c
+++ pidreg.c
@@ -59,8 +59,8 @@ void PID_Stop(PID_Controller_TypeDef *pPID_Controller)
 
 void PID_Processing(PID_Controller_TypeDef *pPID_Controller)
 {
-  //if (pPID_Controller->PID_Controller_Properties & PID_CONTROLLER_ENABLE)
-  //{
+  if (pPID_Controller->PID_Controller_Properties & PID_CONTROLLER_ENABLE)
+  {
         pPID_Controller->CurrentValue = pPID_Controller->PID_CurrentValueUpdateCallback();
 	pPID_Controller->ErrorPreviusValue = pPID_Controller->ErrorT;    
         pPID_Controller->ErrorT = pPID_Controller->TargetValue - pPID_Controller->CurrentValue;
@@ -71,7 +71,7 @@ void PID_Processing(PID_Controller_TypeDef *pPID_Controller)
         pPID_Controller->PID_ControllerOut = pPID_Controller->Reg_Proportional +
                                             pPID_Controller->Reg_Integral +
                                             pPID_Controller->Reg_Derivative;
-  //}
+  }
 }
 
 /*Changing the target value 
