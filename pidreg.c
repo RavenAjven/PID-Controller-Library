a96f0a7ca524297f75eb725014934d52fa30c1d3
Ivan <vpiroman@gmail.com>
2021-09-14 17:11:31 +0300

    Add files via upload

--- pidreg.c
+++ pidreg.c
@@ -2,7 +2,7 @@
 PID Controller library.
 -----------------------------------------------------------------
 Use a PID_Controller_TypeDef to create one instance of controller
-Используйте PID_Controller_TypeDef для создания одного экземпляра контроллера
+РСЃРїРѕР»СЊР·СѓР№С‚Рµ PID_Controller_TypeDef РґР»СЏ СЃРѕР·РґР°РЅРёСЏ РѕРґРЅРѕРіРѕ СЌРєР·РµРјРїР»СЏСЂР° РєРѕРЅС‚СЂРѕР»Р»РµСЂР°
 -----------------------------------------------------------------
 Ivan Kondrashov
 http://github.com/RavenAjven
@@ -14,15 +14,44 @@ vpiroman@gmail.com
 #include <stdint.h>
 #include "pidreg.h"
 
+void PID_Init (PID_Controller_TypeDef *pPID_Controller, float TargetValue, float Error_Ratio, float Prop_Ratio, float Integral_Ratio, float Derivative_Ratio, float (*PID_CurrentValueUpdateCallback)(void))
+{
+pPID_Controller->TargetValue = TargetValue;
+pPID_Controller->Eror_Ratio = Error_Ratio;
+pPID_Controller->Prop_Ratio = Prop_Ratio;
+pPID_Controller->Integral_Ratio = Integral_Ratio;
+pPID_Controller->Derivative_Ratio = Derivative_Ratio;
+pPID_Controller->PID_CurrentValueUpdateCallback = PID_CurrentValueUpdateCallback;
+}
+
+void PID_Deinit(PID_Controller_TypeDef *pPID_Controller)
+{
+PID_Stop(pPID_Controller);
+pPID_Controller->TargetValue = 0;
+pPID_Controller->CurrentValue = 0;
+pPID_Controller->PID_CurrentValueUpdateCallback = 0;
+pPID_Controller->Eror_Ratio = 0;
+pPID_Controller->Prop_Ratio = 0;
+pPID_Controller->Integral = 0;
+pPID_Controller->Integral_Ratio = 0;
+pPID_Controller->Derivative_Ratio = 0;
+pPID_Controller->ErrorT = 0;
+pPID_Controller->ErrorPreviusValue = 0;
+pPID_Controller->Reg_Proportional = 0;
+pPID_Controller->Reg_Integral = 0;
+pPID_Controller->Reg_Derivative = 0;
+pPID_Controller->PID_ControllerOut = 0;
+}
+
 /*Start of the PID controller
-(Cтарт ПИД-Контроллера*/
+(CС‚Р°СЂС‚ РџРР”-РљРѕРЅС‚СЂРѕР»Р»РµСЂР°*/
 void PID_Start(PID_Controller_TypeDef *pPID_Controller)
 {
 	pPID_Controller->PID_Controller_Properties |= PID_CONTROLLER_ENABLE;
 }
 
 /*Stop of the PID controller
-(Cтоп ПИД-Контроллера*/
+(CС‚РѕРї РџРР”-РљРѕРЅС‚СЂРѕР»Р»РµСЂР°*/
 void PID_Stop(PID_Controller_TypeDef *pPID_Controller)
 {
 	pPID_Controller->PID_Controller_Properties &= !PID_CONTROLLER_ENABLE;
@@ -38,41 +67,46 @@ void PID_Processing(PID_Controller_TypeDef *pPID_Controller)
         pPID_Controller->Integral = pPID_Controller->Integral + (pPID_Controller->ErrorT + pPID_Controller->ErrorPreviusValue)/2;
         pPID_Controller->Reg_Integral = pPID_Controller->Integral * (1 / pPID_Controller->Integral_Ratio);
         pPID_Controller->Reg_Derivative = pPID_Controller->Derivative_Ratio*(pPID_Controller->ErrorT - pPID_Controller->ErrorPreviusValue);
-        pPID_Controller->PID_RegulatorOut = pPID_Controller->Reg_Proportional +
+        pPID_Controller->PID_ControllerOut = pPID_Controller->Reg_Proportional +
                                             pPID_Controller->Reg_Integral +
                                             pPID_Controller->Reg_Derivative;
   }
 }
 
 /*Changing the target value 
-(Изменение уставки)*/
-void PID_ChangeTargetValue(PID_Controller_TypeDef *pPID_Controller, float NewTarget)
+(РР·РјРµРЅРµРЅРёРµ СѓСЃС‚Р°РІРєРё)*/
+void PID_SetNewTargetValue(PID_Controller_TypeDef *pPID_Controller, float NewTarget)
 {
 	pPID_Controller->TargetValue = NewTarget;
 }
 
-void PID_ChangeCoefficient(PID_Controller_TypeDef *pPID_Controller, PID_CoefficientList PID_WhatChange, float NewtValue)
+void PID_SetNewCoefficient(PID_Controller_TypeDef *pPID_Controller, PID_CoefficientList PID_WhatChange, float NewtValue)
 {   
-	switch (PID_WhatChange) { /*What change? (Что изменить?)*/
+	switch (PID_WhatChange) { /*What change? (Р§С‚Рѕ РёР·РјРµРЅРёС‚СЊ?)*/
 	case EC_ERROR_COEF: {
-                /*Error Gain Coefficient (Коэффициент усиления ошибки)*/
+                /*Error Gain Coefficient (РљРѕСЌС„С„РёС†РёРµРЅС‚ СѓСЃРёР»РµРЅРёСЏ РѕС€РёР±РєРё)*/
 		pPID_Controller->Eror_Ratio = NewtValue;
 		break;
 	}
 	case PC_PROPORTIONAL_COEF: {
-                /*Proportinal Coefficient (Пропорциональный коэффициент)*/
+                /*Proportinal Coefficient (РџСЂРѕРїРѕСЂС†РёРѕРЅР°Р»СЊРЅС‹Р№ РєРѕСЌС„С„РёС†РёРµРЅС‚)*/
 		pPID_Controller->Prop_Ratio = NewtValue;
 		break;
 	}
 	case IC_INTEGRAL_COEF: {
-                /*Integral Coefficient (Интегральный Коэффициент)*/
+                /*Integral Coefficient (РРЅС‚РµРіСЂР°Р»СЊРЅС‹Р№ РљРѕСЌС„С„РёС†РёРµРЅС‚)*/
 		pPID_Controller->Integral_Ratio = NewtValue;
 		break;
 	}
 	case DC_DERIVATIVE_COEF: {
-                /*Derivative Coefficient (Дифференциальный Коэффициент)*/
+                /*Derivative Coefficient (Р”РёС„С„РµСЂРµРЅС†РёР°Р»СЊРЅС‹Р№ РљРѕСЌС„С„РёС†РёРµРЅС‚)*/
 		pPID_Controller->Derivative_Ratio = NewtValue;
 		break;
 	}
 	}
+}
+
+float PID_GetControllerOut (PID_Controller_TypeDef *pPID_Controller)
+{
+return pPID_Controller->PID_ControllerOut;
 }
--- pidreg.h
+++ pidreg.h
@@ -2,7 +2,7 @@
 PID Controller library.
 -----------------------------------------------------------------
 Use a PID_Controller_TypeDef to create one instance of controller
-Используйте PID_Controller_TypeDef для создания одного экземпляра контроллера
+Eniieucoeoa PID_Controller_TypeDef aey nicaaiey iaiiai yecaiiey?a eiio?ieea?a
 -----------------------------------------------------------------
 Ivan Kondrashov
 http://github.com/RavenAjven
@@ -14,51 +14,46 @@ vpiroman@gmail.com
 #ifndef  PIDREG_H
 #define  PIDREG_H
 
-/*Definition of the "PID Controller" type
-Определение типа "ПИД-Контроллер"*/
+/*Definition of the "PID Controller" type*/
 typedef struct PID_Controller_TypeDef 
 {
-float TargetValue; /*Target Value (Целевое значение для регулятора)*/
-float CurrentValue; /*CurrentValue (Текущее, измеренное значение)*/
+float TargetValue; /*Target Value*/
+float CurrentValue; /*CurrentValue*/
 float (*PID_CurrentValueUpdateCallback) (void);
 /*-------------------------------------------------------------------*/
-/*Coefficients (Коэффициенты)*/
-float Eror_Ratio; /*Error Gain Coefficient (Коэффициент усиления ошибки)*/
-float Prop_Ratio; /*Proportinal Coefficient (Пропорциональный коэффициент)*/
-float Integral; /*Integral Of Error (Интеграл ошибки)*/
-float Integral_Ratio; /*Integral Coefficient (Интегральный Коэффициент)*/
-float Derivative_Ratio; /*Derivative Coefficient (Дифференциальный Коэффициент)*/
+/*Coefficients*/
+float Eror_Ratio; /*Error Gain Coefficient*/
+float Prop_Ratio; /*Proportinal Coefficient*/
+float Integral; /*Integral Of Error*/
+float Integral_Ratio; /*Integral Coefficient*/
+float Derivative_Ratio; /*Derivative Coefficient*/
 /*-------------------------------------------------------------------*/
-/*Components (Составляющие)*/
-float ErrorT; /*Error (Ошибка рассогласования)*/
-float ErrorPreviusValue; /*Previus Error (Предыдущее значение ошибки)*/
-float Reg_Proportional; /*Proportinal Component (Пропорциональная составляющая регулятора)*/
-float Reg_Integral; /*Integral Component (Интегральная составляющая регулятора)*/
-float Reg_Derivative; /*Derivative Component (Дифференциальная составляющая регулятора)*/
+/*Components*/
+float ErrorT; /*Error*/
+float ErrorPreviusValue; /*Previus Error*/
+float Reg_Proportional; /*Proportinal Component*/
+float Reg_Integral; /*Integral Component*/
+float Reg_Derivative; /*Derivative Component*/
 /*-------------------------------------------------------------------*/
-float PID_RegulatorOut; /*Выход регулятора*/
+float PID_ControllerOut; /*PID_ControllerOut*/
 
 /*Properties and flags of the controller instance
 This uses the individual bits in the variable "PID_Controller_Properties"
-Свойства и флаги экземляра ПИД-контроллера
-Используются отдельные биты в uint8_t переменной "PID_Controller_Properties"
-
-[0] - Enabled/Disabled (Контроллер: Активен/Не активен)
-[1] - Reserved (Зарезервирован)
-[2] - Reserved (Зарезервирован)
-[3] - Reserved (Зарезервирован)
-[4] - Reserved (Зарезервирован)
-[5] - Reserved (Зарезервирован)
-[6] - Reserved (Зарезервирован)
-[7] - Reserved (Зарезервирован)
+[0] - Enabled/Disabled
+[1] - Reserved
+[2] - Reserved
+[3] - Reserved
+[4] - Reserved
+[5] - Reserved
+[6] - Reserved
+[7] - Reserved
 */
 uint8_t PID_Controller_Properties;
 #define PID_CONTROLLER_ENABLE 0x01
 /*-------------------------------------------------------------------*/
 } PID_Controller_TypeDef;
 
-/*List of coefficients
-Список коэффициентов*/
+/*List of coefficients*/
 typedef enum 
 {
 EC_ERROR_COEF,
@@ -67,29 +62,25 @@ IC_INTEGRAL_COEF,
 DC_DERIVATIVE_COEF
 } PID_CoefficientList;
 
-/*Start of the PID controller
-(Cтарт ПИД-Контроллера)*/
+/*Start of the PID controller*/
 void PID_Start (PID_Controller_TypeDef *pPID_Controller);
 
-/*Stop of the PID controller
-(Стоп ПИД-Контроллера)*/
+/*Stop of the PID controller*/
 void PID_Stop(PID_Controller_TypeDef *pPID_Controller);
 
-void PID_Init (void);
+void PID_Init (PID_Controller_TypeDef *pPID_Controller, float TargetValue, float Error_Ratio, float Prop_Ratio, float Integral_Ratio, float Derivative_Ratio, float (*PID_CurrentValueUpdateCallback)(void));
 
-void PID_Deinit(void);
+void PID_Deinit(PID_Controller_TypeDef *pPID_Controller);
 
-/*Call it in a program loop
-(Вызывать в цикле программы)*/
+float PID_GetControllerOut (PID_Controller_TypeDef *pPID_Controller);
+
+/*Call it in a program loop*/
 void PID_Processing (PID_Controller_TypeDef *pPID_Controller);
 
-/*Changing the target value 
-(Изменение уставки)*/
-void PID_ChangeTargetValue (PID_Controller_TypeDef *pPID_Controller, float NewTarget);
+/*Changing the target value*/
+void PID_SetNewTargetValue (PID_Controller_TypeDef *pPID_Controller, float NewTarget);
 
-/*Changing the coefficient value
-(Изменение коэффициента)
-*/
-void PID_ChangeCoefficient(PID_Controller_TypeDef *pPID_Controller, PID_CoefficientList PID_WhatChange, float NewtValue);
+/*Changing the coefficient value*/
+void PID_SetNewCoefficient(PID_Controller_TypeDef *pPID_Controller, PID_CoefficientList PID_WhatChange, float NewtValue);
 
 #endif
